74e48255d879997e4fc6197a1cf9ad75f13fc701
juligarji <jgarzonj@unal.edu.co>
2018-04-12 07:59:13 -0500

    agregando versiones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+import { UserComponent } from '../app/user/user.component'
+import { ListComponent } from '../app/list/list.component'
+
+const appRoutes: Routes = [
+  { path: 'lista/:page', component: ListComponent },
+  { path: 'usuario',      component: UserComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,UserComponent,ListComponent]
 })
 export class AppModule { }
